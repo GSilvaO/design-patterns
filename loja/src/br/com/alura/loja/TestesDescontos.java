eb9bc6e086e6bcba802d33901a86ce3d1322310f
GSilvaO <gsoliveira.x@gmail.com>
2024-01-24 13:01:13 -0300

    alteracao da classe TestesDescontos para se comportar conforme a refatoracao feita anteriormente na classe de Orcamento com base na criacao da interface Orcavel e da classe ItemOrcamento. Remocao do código de Tread.sleep da classe orcamento ao pegar um valor

--- loja/src/br/com/alura/loja/TestesDescontos.java
+++ loja/src/br/com/alura/loja/TestesDescontos.java
@@ -3,16 +3,26 @@ package br.com.alura.loja;
 import java.math.BigDecimal;
 
 import br.com.alura.loja.desconto.CalculadoraDeDescontos;
+import br.com.alura.loja.orcamento.ItemOrcamento;
 import br.com.alura.loja.orcamento.Orcamento;
 
 public class TestesDescontos {
 	public static void main(String[] args) {
-		Orcamento primeiro = new Orcamento(new BigDecimal("400"), 6);
-		Orcamento segundo = new Orcamento(new BigDecimal("1000"), 1);
+
+		ItemOrcamento primeiroItem = new ItemOrcamento(new BigDecimal("400"));
+		ItemOrcamento segundoItem = new ItemOrcamento(new BigDecimal("1000"));
+		Orcamento orcamentoPrimeiroItem = new Orcamento();
+		Orcamento orcamentoSegundoItem = new Orcamento();
 		
 		CalculadoraDeDescontos calculadora = new CalculadoraDeDescontos();
+
+		for(int i = 0; i < 5; i++) {
+			orcamentoPrimeiroItem.adicionarItem(primeiroItem);
+		}
+
+		orcamentoSegundoItem.adicionarItem(segundoItem);
 		
-		System.out.println(calculadora.calcular(primeiro));
-		System.out.println(calculadora.calcular(segundo));
+		System.out.println(calculadora.calcular(orcamentoPrimeiroItem));
+		System.out.println(calculadora.calcular(orcamentoSegundoItem));
 	}
 }
