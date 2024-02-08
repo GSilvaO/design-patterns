00bcf00bf28e99a5a20481b89daa8d030c737a78
Gabriel Oliveira <gsoliveira.x@gmail.com>
2024-02-08 09:34:35 -0300

    adicao de documentacao na classe CalculadoraDeDescontos do pacote desconto

--- loja/src/br/com/alura/loja/desconto/CalculadoraDeDescontos.java
+++ loja/src/br/com/alura/loja/desconto/CalculadoraDeDescontos.java
@@ -4,6 +4,11 @@ import java.math.BigDecimal;
 
 import br.com.alura.loja.orcamento.Orcamento;
 
+/**
+ * Classe cliente responsável por instanciar os descontos na ordem em
+ * que devem ser aplicados. Para isto é implementado o design pattern
+ * de Chain of Responsability
+ */
 public class CalculadoraDeDescontos {
 
 	public BigDecimal calcular(Orcamento orcamento) {
